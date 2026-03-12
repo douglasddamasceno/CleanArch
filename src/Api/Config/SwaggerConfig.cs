707fc5d6d4250c665639641bc56fd7e1a8e1e602
Douglas Damasceno <douglasddx@outlook.com>
2026-03-11 21:52:36 -0300

    add>

--- src/Api/Config/SwaggerConfig.cs
+++ src/Api/Config/SwaggerConfig.cs
@@ -1,43 +1,43 @@
-using Microsoft.OpenApi.Models;
+// using Microsoft.OpenApi.Models;
 
-namespace CleanArch.Api.Config;
+// namespace CleanArch.Api.Config;
 
-public static class SwaggerConfig
-{
-    public static void AddSwaggerConfig(this IServiceCollection services)
-    {
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen(c =>
-        {
-            c.SwaggerDoc("v1",
-                new OpenApiInfo
-                {
-                    Title = "Clean Architecture",
-                    Version = "v1.0",
-                    Description = "Base arquitetural para aplicações .NET seguindo os princípios da Arquitetura Limpa (Clean Arch).",
-                    Contact = new OpenApiContact
-                    {
-                        Name = "Douglas Damasceno",
-                        Email = "douglasddx@gmail.com",
-                        Url = new Uri("https://github.com/douglasddamasceno/")
-                    },
-                    License = new OpenApiLicense
-                    {
-                        Name = "MIT License",
-                        Url = new Uri("https://opensource.org/licenses/MIT")
-                    }
-                }
-            );
-        });
-    }
+// public static class SwaggerConfig
+// {
+//     public static void AddSwaggerConfig(this IServiceCollection services)
+//     {
+//         services.AddEndpointsApiExplorer();
+//         services.AddSwaggerGen(c =>
+//         {
+//             c.SwaggerDoc("v1",
+//                 new OpenApiInfo
+//                 {
+//                     Title = "Clean Architecture",
+//                     Version = "v1.0",
+//                     Description = "Base arquitetural para aplicações .NET seguindo os princípios da Arquitetura Limpa (Clean Arch).",
+//                     Contact = new OpenApiContact
+//                     {
+//                         Name = "Douglas Damasceno",
+//                         Email = "douglasddx@gmail.com",
+//                         Url = new Uri("https://github.com/douglasddamasceno/")
+//                     },
+//                     License = new OpenApiLicense
+//                     {
+//                         Name = "MIT License",
+//                         Url = new Uri("https://opensource.org/licenses/MIT")
+//                     }
+//                 }
+//             );
+//         });
+//     }
 
-    public static void UseSwaggerConfig(this WebApplication app)
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(c =>
-        {
-            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean Arch API v1");
-            c.RoutePrefix = string.Empty;
-        });
-    }
-}
+//     public static void UseSwaggerConfig(this WebApplication app)
+//     {
+//         app.UseSwagger();
+//         app.UseSwaggerUI(c =>
+//         {
+//             c.SwaggerEndpoint("/swagger/v1/swagger.json", "Clean Arch API v1");
+//             c.RoutePrefix = string.Empty;
+//         });
+//     }
+// }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -2,23 +2,29 @@ using Api.Endpoints;
 using Application.Interfaces;
 using Application.Services;
 using CleanArch.Api.Config;
+
+//using CleanArch.Api.Config;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDocsConfig();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LojaConnection")));
 builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
 builder.Services.AddScoped<ProdutoService>();
-builder.Services.AddSwaggerConfig();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
-app.UseSwaggerConfig();
 app.UseHealthChecks("/health");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDocsConfig();
+}
 
 app.MapProdutoEndpoints();
 
